2ab12ebfa0a4054a74679d0cd97d8f65509b7f45
Gaurav Kumar Singh <rajgaurav99@gmail.com>
2020-04-29 00:07:35 +0530

    Heat Map improvement and CDN

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,6 +11,12 @@
    promise.then(function (response) {
      var statedata={};
      ctrl.coviddata=response.data.regional;
+     var max=0;
+     for(var i=0;i<ctrl.coviddata.length;i++){
+       if(ctrl.coviddata[i].totalConfirmed>max){
+         max=ctrl.coviddata[i].totalConfirmed;
+       }
+     }
      for(var i=0;i<ctrl.coviddata.length;i++){
        var statename=ctrl.coviddata[i].loc;
        var statecode=statelist[statename];
@@ -20,12 +26,13 @@
            text: statename+': '+totalConfirmed,
            backgroundColor: 'white'
          },
-         backgroundColor: "rgba(255, 99, 71,"+(0.2+totalConfirmed/4000)+")" ,
+         backgroundColor: "rgba(255, 99, 71,"+(0.2+totalConfirmed/max)+")" ,
          label: {
            visible: false
          }
        };
      }
+     document.getElementById("grad").style.backgroundImage = "linear-gradient(to right, rgba(255,99,71,0.2) , rgba(255,99,71,1))";
      ctrl.summary=response.data.summary;
      var date=new Date(Date.parse(response.lastRefreshed))
      ctrl.lastRefreshed=date.toString();
