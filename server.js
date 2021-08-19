53eaea44d98bb0b1562e5f634ff231b8d93dd202
Gaurav Kumar Singh <rajgaurav99@gmail.com>
2021-08-19 22:34:19 +0530

    CORS #2

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 app.get('*', function(req, res) {
   res.sendFile('./public/index.html', { root: __dirname })
 });
+
+
+app.all('/*', function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+    response.header("Access-Control-Allow-Methods", "GET, POST", "PUT", "DELETE");
+    next();
+});
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
   console.log('listening on %d', port);
